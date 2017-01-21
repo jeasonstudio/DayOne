714ffb28960c551e9a28f04e669cb82ef4364f12
赵吉彤 <zjtong3576@sina.com>
2017-01-21 21:04:04 +0800

    d

--- model/mail.go
+++ model/mail.go
@@ -22,10 +22,12 @@ func sendEmail() {
     m.SetHeader("To",  // 收件人
         m.FormatAddress("zjtong3576@sina.com", "乔峰"),
     )
+
     m.SetHeader("这是", "啥啊")  // 主题
     m.SetBody("text/html", "Hello <a href = \"http://blog.csdn.net/liang19890820\">一去丶二三里</a>")  // 正文
 
     d := gomail.NewPlainDialer( HOST, 25, USER , PASSWORD)  // 发送邮件服务器、端口、发件人账号、发件人密码
+    
     if err := d.DialAndSend(m); err != nil {
         panic(err)
     }

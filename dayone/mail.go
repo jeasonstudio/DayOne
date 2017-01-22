214c734bde6b8c5f14afc73586a7b8284a917ef3
赵吉彤 <zjtong3576@sina.com>
2017-01-22 13:45:06 +0800

    get

--- dayone/mail.go
+++ dayone/mail.go
@@ -1,34 +1,39 @@
 package dayone
 
 import (
+	f "fmt"
+
 	"gopkg.in/gomail.v2"
 )
 
 const (
-	HOST       = "smtp.exmail.qq.com"
-	SERVERADDR = "smtp.exmail.qq.com:25"
-	ALIAS      = "每天一题"
-	SUBJECT    = "每天一题"
-	USER       = "mailbyjeason@jeasonstudio.cn" //发送邮件的邮箱
-	PASSWORD   = "Admin12345"                   //发送邮件邮箱的密码
+	HOST     = "smtp.exmail.qq.com" //HOST 邮件服务器
+	PORT     = 25
+	SUBJECT  = "每日一题"
+	USER     = "mailbyjeason@jeasonstudio.cn"
+	PASSWORD = "Admin12345"
 )
 
-func sendEmail() string {
+// SendEmail 发送邮件
+func SendEmail(innerHTML string) {
 
 	m := gomail.NewMessage()
-	m.SetAddressHeader("From", USER, "一去、二三里") // 发件人
-	m.SetHeader("To",                          // 收件人
-		m.FormatAddress("zjtong3576@sina.com", "乔峰"),
+	m.SetAddressHeader("From", USER, "Jeason")
+	m.SetHeader("To",
+		m.FormatAddress("zjtong3576@sina.com", "Jeason"),
 	)
 
-	m.SetHeader("这是", "啥啊")                                                                     // 主题
-	m.SetBody("text/html", "Hello <a href = \"http://blog.csdn.net/liang19890820\">一去丶二三里</a>") // 正文
+	m.SetHeader("Subject", SUBJECT)
+	m.SetBody("text/html", innerHTML)
 
-	d := gomail.NewPlainDialer(HOST, 25, USER, PASSWORD) // 发送邮件服务器、端口、发件人账号、发件人密码
+	f.Println("Send Ready...")
+
+	d := gomail.NewPlainDialer(HOST, PORT, USER, PASSWORD)
+	// 发送邮件服务器、端口、发件人账号、发件人密码
 
 	if err := d.DialAndSend(m); err != nil {
+		f.Println("Send Fail with,", err)
 		panic(err)
 	}
-
-	return "ok"
+	f.Println("Send Email Success!!")
 }
--- main.go
+++ main.go
@@ -1,42 +1,10 @@
 package main
 
 import (
-	"fmt"
-
-	"gopkg.in/gomail.v2"
+	dayone "DayOne/dayone"
 )
-
-const (
-	SERVICE  = "smtp.exmail.qq.com"
-	PORT     = 25
-	ALIAS    = "每天一题"
-	SUBJECT  = "每天一题"
-	USER     = "mailbyjeason@jeasonstudio.cn" //发送邮件的邮箱
-	PASSWORD = "Admin12345"                   //发送邮件邮箱的密码
-)
-
-func sendEmail() {
-
-	m := gomail.NewMessage()
-	m.SetAddressHeader("From", USER, "Jeason") // 发件人
-	m.SetHeader("To", "zjtong3576@sina.com", "me@jeasonstudio.cn")
-
-	m.SetHeader("Subject", ALIAS)
-	m.SetBody("text/html", "Hello <a href = \"http://blog.csdn.net/liang19890820\">一去丶二三里</a>") // 正文
-
-	fmt.Println("Ready to sendEmail...")
-
-	d := gomail.NewPlainDialer(SERVICE, PORT, USER, PASSWORD) // 发送邮件服务器、端口、发件人账号、发件人密码
-
-	if err := d.DialAndSend(m); err != nil {
-		fmt.Println("Sending err,", err)
-		panic(err)
-	}
-	fmt.Println("Send Success!")
-
-}
 
 func main() {
-	sendEmail()
+	dayone.SendEmail("TEST")
 	// fmt.Println(a)
 }
